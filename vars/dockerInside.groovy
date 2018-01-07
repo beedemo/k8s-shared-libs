d6db32e8f19a564d25fc522e2b7536757778cc1b
Kurt Madel <kmadel@mac.com>
2018-01-07 12:42:03 -0500

    added node block

--- vars/dockerInside.groovy
+++ vars/dockerInside.groovy
@@ -3,8 +3,10 @@
 def call(String image, Closure body) {
   podTemplate(label: 'kubernetes',
     containers: [containerTemplate(name: "$image", image: "$image", command: 'cat', ttyEnabled: true)]) {
-    container("$image") {
-      body()
+    node('kubernetes') {
+      container("$image") {
+        body()
+      }
     }
   }
 }
