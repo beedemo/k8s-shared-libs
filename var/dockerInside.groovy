7598df1d913efb45368e4e11f4e7e767ecbe635a
Kurt Madel <kmadel@mac.com>
2018-01-07 11:38:07 -0500

    Delete dockerInside.groovy

--- var/dockerInside.groovy
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/groovy
-def call(image) {
-  podTemplate(label: kubernetes,
-    containers: [containerTemplate(name: "$image", image: "$image", command: 'cat', ttyEnabled: true)]) {
-    container("$image") {
-      body()
-    }
-  }
-}
