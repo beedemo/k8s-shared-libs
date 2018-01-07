1f19d5f7a70958c48b271957673194d62a06829f
Kurt Madel <kmadel@mac.com>
2018-01-07 11:37:55 -0500

    Create dockerInside.groovy

---
+++ vars/dockerInside.groovy
@@ -0,0 +1,9 @@
+#!/usr/bin/groovy
+def call(image) {
+  podTemplate(label: kubernetes,
+    containers: [containerTemplate(name: "$image", image: "$image", command: 'cat', ttyEnabled: true)]) {
+    container("$image") {
+      body()
+    }
+  }
+}
