4e2f8fda9c7bddf4fd2cd01273c7d3b3ad347708
Kurt Madel <kmadel@mac.com>
2018-01-07 11:28:53 -0500

    Update dockerInside.groovy

--- var/dockerInside.groovy
+++ var/dockerInside.groovy
@@ -1,1 +1,9 @@
-
+#!/usr/bin/groovy
+def call(image) {
+  podTemplate(label: kubernetes,
+    containers: [containerTemplate(name: "$image", image: "$image", command: 'cat', ttyEnabled: true)]) {
+    container("$image") {
+      body()
+    }
+  }
+}
