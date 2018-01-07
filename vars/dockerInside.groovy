fd86de352e5813b675bb66d7cd44f658a35b436a
Kurt Madel <kmadel@mac.com>
2018-01-07 13:33:57 -0500

    fix image

--- vars/dockerInside.groovy
+++ vars/dockerInside.groovy
@@ -1,8 +1,8 @@
 #!/usr/bin/groovy
 
-def call(String name, Closure body) {
+def call(String image, Closure body) {
   podTemplate(label: 'kubernetes',
-    containers: [containerTemplate(name: "name", image: "$imageName:$imageTag", command: 'cat', ttyEnabled: true)]) {
+    containers: [containerTemplate(name: "name", image: "$image", command: 'cat', ttyEnabled: true)]) {
     node('kubernetes') {
       container("name") {
         body()
